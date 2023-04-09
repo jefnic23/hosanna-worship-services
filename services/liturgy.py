195520a2c17dba05afd5f00413a9353b997862f7
Jeff Nicholas <jefnic23@gmail.com>
2023-04-09 00:18:04 -0400

    🔀 refactor(liturgy.py): refactor Liturgy class to use pathlib and simplify file loading The Liturgy class has been refactored to use pathlib instead of string concatenation to build file paths. The _open_files method has been added to simplify the loading of liturgy files. The __getattr__ method has been added to allow for dynamic attribute access to the liturgy files.

--- services/liturgy.py
+++ services/liturgy.py
@@ -1,48 +1,33 @@
 import os
+from pathlib import Path
 
 
 class Liturgy():
     '''Loads and stores the liturgy.'''
 
+    PATH = Path('D:/Documents/Hosanna/liturgy')
+
+
     def __init__(self):
-        if os.path.exists('liturgy/confession.txt'):
-            self.confession = open('liturgy/confession.txt', 'r', encoding='utf-8').read()
+        self._files = Liturgy._open_files()
+        
+    
+    @staticmethod
+    def _open_files(path=PATH):
+        '''Open all the liturgy files.'''
+        files = {}
+        for file in os.listdir(path):
+            if file.endswith('.txt'):
+                files[Path(file).stem] = open(
+                    f'{path}/{file}', 
+                    'r', 
+                    encoding='utf-8'
+                ).read()
+        return files
+    
 
-        if os.path.exists('liturgy/greeting.txt'):
-            self.greeting = open('liturgy/greeting.txt', 'r', encoding='utf-8').read()
-
-        if os.path.exists('liturgy/kyrie.txt'):
-            self.kyrie = open('liturgy/kyrie.txt', 'r', encoding='utf-8').read()
-
-        if os.path.exists('liturgy/creed.txt'):
-            self.creed = open('liturgy/creed.txt', 'r', encoding='utf-8').read()
-
-        if os.path.exists('liturgy/dialogue.txt'):
-            self.dialogue = open('liturgy/dialogue.txt', 'r', encoding='utf-8').read()
-
-        if os.path.exists('liturgy/holy-holy-holy.txt'):
-            self.hosanna = open('liturgy/holy-holy-holy.txt', 'r', encoding='utf-8').read()
-
-        if os.path.exists('liturgy/communion-dialogue.txt'):
-            self.communion_dialogue = open('liturgy/communion-dialogue.txt', 'r', encoding='utf-8').read()
-
-        if os.path.exists('liturgy/lord-be-with-you.txt'):
-            self.lord_be_with_you = open('liturgy/lord-be-with-you.txt', 'r', encoding='utf-8').read()
-
-        if os.path.exists('liturgy/lords-prayer.txt'):
-            self.lords_prayer = open('liturgy/lords-prayer.txt', 'r', encoding='utf-8').read()
-
-        if os.path.exists('liturgy/communion-hymn.txt'):
-            self.communion_hymn = open('liturgy/communion-hymn.txt', 'r', encoding='utf-8').read()
-
-        if os.path.exists('liturgy/gospel-acclamation.txt'):
-            self.gospel_acclamation = open('liturgy/gospel-acclamation.txt', 'r', encoding='utf-8').read()
-
-        if os.path.exists('liturgy/prayer-after-communion.txt'):
-            self.prayer_after_communion = open('liturgy/prayer-after-communion.txt', 'r', encoding='utf-8').read()
-
-        if os.path.exists('liturgy/benediction.txt'):
-            self.benediction = open('liturgy/benediction.txt', 'r', encoding='utf-8').read()
-
-        if os.path.exists('liturgy/dismissal.txt'):
-            self.dismissal = open('liturgy/dismissal.txt', 'r', encoding='utf-8').read()
+    def __getattr__(self, name):
+        '''Return the liturgy file.'''
+        if name in self._files:
+            return self._files[name]
+        raise AttributeError(f'{name} not found')
