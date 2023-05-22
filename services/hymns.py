86db93fa6a767aa804b9d52ad50d09ac00ec7d45
Jeff Nicholas <jefnic23@gmail.com>
2023-05-22 18:51:34 -0400

    refactor(hymnal.csv): change column name from 'Hymn' to 'Number' to improve semantics refactor(hymns.py): remove unused imports and methods, use pandas DataFrame instead of Redis to store hymns, update _get_hymn method to use DataFrame instead of Redis to retrieve hymns

--- services/hymns.py
+++ services/hymns.py
@@ -1,8 +1,6 @@
 from datetime import date
 
-import pandas
-import redis
-import csv
+import pandas as pd
 
 from config import settings
 from models.hymn import Hymn
@@ -13,15 +11,12 @@ class Hymns:
 
 
     LOCAL_DIR = settings.LOCAL_DIR
-    DF = pandas.read_csv('data/hymnal.csv', index_col='Hymn')
-    R = redis.Redis()
+    DF = pd.read_csv('data/hymnal.csv', index_col='Number')
 
 
     def __init__(self) -> None:
         self.day: date = date.today()
         self._hymns: list[Hymn] = []
-        
-        self._load_hymns()
     
 
     def add_hymn(self, hymn_number: int) -> None:
@@ -38,22 +33,15 @@ class Hymns:
             for hymn in self._hymns:
                 f.write(f'{hymn.Title}\nELW {hymn.Number}\n')
             f.close()
-            
-            
-    def _load_hymns(self) -> None:
-        with open('data/hymnal.csv', 'r') as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                self.R.set(row['Hymn'], row['Title'])
 
 
     @staticmethod
     def _get_hymn(
         hymn_number: int, 
-        r: redis.Redis = R
+        df: pd.DataFrame = DF
     ) -> object:
         """Get hymn by hymn number."""
         try:
-            return r.get(str(hymn_number))
+            return df.loc[hymn_number]
         except KeyError:
             print(f'Hymn {hymn_number} not found.')
