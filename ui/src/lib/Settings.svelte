e36a8b3fd43ed6063866dafe27c6f66153e1af2f
Jeff Nicholas <jnicholas@hamiltonmfg.com>
2023-05-23 13:26:42 -0400

    🐛 fix(Settings.svelte): fix Settings component to allow user to change LOCAL_DIR setting ✨ feat(Settings.svelte): add button to choose directory for LOCAL_DIR setting ✨ feat(Settings.svelte): add functionality to update LOCAL_DIR setting when user chooses a directory ✨ feat(Settings.svelte): add save button to update all settings at once ✨ feat(types/settings.ts): add interface for Settings object ✨ feat(main.ts): add showDirectoryPicker to global window object to allow access to directory picker API

--- ui/src/lib/Settings.svelte
+++ ui/src/lib/Settings.svelte
@@ -1,13 +1,34 @@
 <script lang="ts">
     import { eel } from "../main";
+    import { type Settings } from "./types/settings";
 
-    let settings: object = {};
+    let settings: Settings = {};
 
-    eel.get_settings()((s: object) => {
+    eel.get_settings()((s: Settings) => {
         settings = s;
     });
+
+    const handleChange = (e: Event) => {
+        settings[(e.target as HTMLInputElement).name] = (e.target as HTMLInputElement).value;
+    }
+
+    async function getDir() {
+        const handleDir = await window.showDirectoryPicker();
+        const dir = await handleDir.getDirectory();
+        const path = dir.path;
+        settings["LOCAL_DIR"] = path;
+    }
 </script>
 
 {#each Object.keys(settings) as setting}
-    <p>{setting}: {settings[setting]}</p>
+    {@const input_type = setting === "LOCAL_DIR" ? "button" : "text"}
+    <span>
+        <p>{setting}:</p>
+        {#if input_type === "button"}
+            <button on:click={getDir}>Choose Directory</button>
+        {:else}
+            <input type={input_type} name={setting} value={settings[setting]} on:change={handleChange} />
+        {/if}
+    </span>
 {/each}
+<button on:click={() => eel.update_settings(settings)}>Save</button>
