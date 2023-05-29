eb67579f60b453a49da1ee8f8837088d931541a9
Jeff Nicholas <jefnic23@gmail.com>
2023-05-29 18:26:13 -0400

    🎨 style(Planner.svelte): add styling to planner elements for better readability and separation of elements 🔨 chore(Planner.svelte): change element names to title case for better consistency and readability

--- ui/src/lib/Planner.svelte
+++ ui/src/lib/Planner.svelte
@@ -1,30 +1,30 @@
 <script lang="ts">
     const elements: {} = {
-        'confession': 'call_and_response',
-        'gathering_song': 'hymn',
-        'greeting': 'call_and_response',
-        'kyrie': 'congregation',
-        'prayer_of_the_day': 'call_and_response',
-        'first_reading': 'reading',
-        'psalm': 'psalm',
-        'second_reading': 'reading',
-        'gospel_acclamation': 'congregation',
-        'gospel': 'gospel',
-        'sermon': 'title',
-        'hym_of_the_day': 'hymn',
-        'creed': 'congregation',
-        'prayers_of_intercession': 'intercessions',
-        'dialogue': 'call_and_response',
-        'holy_holy_holy': 'congregation',
-        'thanksgiving': 'call_and_response',
-        'lords_prayer': 'congregation',
-        'communion': 'title',
-        'communion_song': 'congregation',
-        'communion_blessing': 'call_and_response',
-        'prayer_after_communion': 'call_and_response',
-        'blessing': 'call_and_response',
-        'sending_song': 'hymn',
-        'dismissal': 'call_and_response'
+        'Confession': 'call_and_response',
+        'Gathering Song': 'hymn',
+        'Greeting': 'call_and_response',
+        'Kyrie': 'congregation',
+        'Prayer of the Day': 'call_and_response',
+        'First Reading': 'reading',
+        'Psalm': 'psalm',
+        'Second Reading': 'reading',
+        'Gospel Acclamation': 'congregation',
+        'Gospel': 'gospel',
+        'Sermon': 'title',
+        'Hymn of the Day': 'hymn',
+        'Creed': 'congregation',
+        'Prayers of Intercession': 'intercessions',
+        'Dialogue': 'call_and_response',
+        'Holy, Holy, Holy': 'congregation',
+        'Thanksgiving': 'call_and_response',
+        'Lord\'s Prayer': 'congregation',
+        'Communion': 'title',
+        'Communion Song': 'congregation',
+        'Communion Blessing': 'call_and_response',
+        'Prayer after Communion': 'call_and_response',
+        'Blessing': 'call_and_response',
+        'Sending Song': 'hymn',
+        'Dismissal': 'call_and_response'
     }
 </script>
 
@@ -34,3 +34,15 @@
         <div class="planner-element-type">{elements[element]}</div>
     </div>
 {/each}
+
+<style>
+    .planner-element {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+        height: 50px;
+        border-bottom: 1px solid black;
+    }
+</style>
