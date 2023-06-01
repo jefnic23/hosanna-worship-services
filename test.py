06b6312c721a29166e89107de7ce30b09339a8f2
Jeff Nicholas <jefnic23@gmail.com>
2023-05-31 22:54:31 -0400

    🐛 fix(test.py): import bs4 module to fix NameError when running the script ✨ feat(test.py): add function to add superscripts to text using <sup> tags to improve readability of text with superscripts

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import re
 
 from bs4 import BeautifulSoup
+import bs4
 
 from services.utils import (clean_text, get_superscripts, grouper,
                             split_formatted_text)
@@ -8,6 +9,8 @@ from services.utils import (clean_text, get_superscripts, grouper,
 with open('data/test.html', 'rb') as f:
     soup = BeautifulSoup(f.read(), 'html.parser')
 
+
+# get superscripts from soup
 superscripts = [
     superscript.get_text() for superscript in soup.find_all('sup', {'class': None})
 ]
@@ -16,19 +19,33 @@ for i, s in enumerate(superscripts):
         superscripts[i] = f'{s}{superscripts[i + 1]}'
         superscripts.pop(i + 1)
 
+# get text from soup
 text = '\n'.join([clean_text(ele) for ele in soup.get_text().splitlines()])
 
-# replace superscripts in text with <sup> tags
-for s in superscripts:
-    m = re.search(s, text)
-    print(m, s)
-    if m:
-        text = text.replace(s, f'<sup>{s}</sup>')
+def find_superscript(text: str, superscript: str, start: int = 0):
+    '''Find the start and end index of a superscript in a string'''
+    length = len(superscript)
+    index = text.find(superscript, start)
+    return index, index + length
 
-# superscripts = re.findall(r'<sup>(.*?)</sup>', text)
 
-# print(text)
+def add_superscripts_to_text(text: str, superscripts: list) -> str:
+    def find_superscript(text: str, superscript: str, start: int = 0):
+        '''Find the start and end index of a superscript in a string'''
+        length = len(superscript)
+        index = text.find(superscript, start)
+        return index, index + length
 
+    start = 0
+    new_text = ""
+    for superscript in superscripts:
+        indeces = find_superscript(text, superscript, start)
+        new_text += text[start:indeces[0]] + f'<sup>{superscript}</sup>'
+        start = indeces[1]
+    new_text += text[start:]
+    return new_text
+
+print(add_superscripts_to_text(text, superscripts))
 
 # spans = [
 #     clean_text(span.get_text()) 
