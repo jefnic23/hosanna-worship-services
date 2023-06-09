685ebb49cdb559a87e84b5eee75872df3bf0f1bf
Jeff Nicholas <jefnic23@gmail.com>
2023-06-08 22:37:25 -0400

    ✨ feat(Tracker.svelte): add Tracker component to handle navigation between tabs in the app

---
+++ ui/src/components/Tracker.svelte
@@ -0,0 +1,37 @@
+<script lang='ts'>
+    import { activeTab } from "../stores";
+
+    const handleClick = (tab: string) => {
+        activeTab.set(tab);
+    }
+</script>
+
+<ul>
+    <li>
+        <span on:click={() => handleClick('date')}>Select date</span>
+    </li>
+    <li>
+        <span on:click={() => handleClick('liturgy')}>Select liturgy</span>
+    </li>
+    <li>
+        <span on:click={() => handleClick('powerpoint')}>Build PowerPoint</span>
+    </li>
+    <li>
+        <span on:click={() => handleClick('review')}>Review & confirm</span>
+    </li>
+</ul>
+
+<style>
+    ul {
+        padding-left: 1.2rem;
+        list-style: circle;
+    }
+
+    li {
+        margin: 1rem 0;
+    }
+
+    span:hover {
+        cursor: pointer;
+    }
+</style>
