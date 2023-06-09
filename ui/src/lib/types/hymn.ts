4b8ec7b7532c05c598b6fd85ef08af1ec9113f84
Jeff Nicholas <jefnic23@gmail.com>
2023-06-08 22:36:07 -0400

    reorganized file structure

--- ui/src/lib/types/hymn.ts
+++
@@ -1,4 +0,0 @@
-export interface Hymn {
-    Number: number;
-    Title: string;
-}
--- ui/src/lib/types/settings.ts
+++
@@ -1,8 +0,0 @@
-export interface Settings {
-    USER?: string;
-    PASSWORD?: string;
-    DROPBOX_APP_KEY?: string;
-    DROPBOX_APP_SECRET?: string;
-    DROPBOX_REFRESH_TOKEN?: string;
-    LOCAL_DIR?: string;
-}
