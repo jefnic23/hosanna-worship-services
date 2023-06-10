214ff458dca4d99f6b9db08a4b811372cc0903f8
Jeff Nicholas <jefnic23@gmail.com>
2023-06-09 23:45:53 -0400

    🔧 chore(stores.ts): add type to activeTab store to improve type safety and readability

--- ui/src/stores.ts
+++ ui/src/stores.ts
@@ -1,3 +1,4 @@
+import type { Writable } from 'svelte/store';
 import { writable } from 'svelte/store';
 
-export const activeTab = writable('date');
+export const activeTab: Writable<string> = writable('date');
