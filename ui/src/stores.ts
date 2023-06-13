7bde4dc82719cfbe6a7edbbe3db8b2271f9a081a
Jeff Nicholas <jnicholas@hamiltonmfg.com>
2023-06-13 12:53:35 -0400

    ✨ feat(stores.ts): add pptElements store to keep track of elements in the ppt slide

--- ui/src/stores.ts
+++ ui/src/stores.ts
@@ -2,3 +2,4 @@ import type { Writable } from 'svelte/store';
 import { writable } from 'svelte/store';
 
 export const activeTab: Writable<string> = writable('date');
+export const pptElements: Writable<string[]> = writable([]);
