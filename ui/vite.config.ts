eb961b40e90f2b960f3e68bd4448927feb74d69f
Jeff Nicholas <jnicholas@hamiltonmfg.com>
2023-06-13 13:30:30 -0400

    🔧 chore(ui): add path aliases to tsconfig.json and vite.config.ts to improve imports and code readability

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +9,11 @@ export default defineConfig({
         emptyOutDir: true,
 	},
 	plugins: [svelte()],
+	resolve: {
+		alias: {
+			'@components': path.resolve(__dirname, './src/components'),
+			'@stores': path.resolve(__dirname, './src/stores'),
+			'@types': path.resolve(__dirname, './src/types'),
+		}
+	}
 })
