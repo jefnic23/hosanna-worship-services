c19395141f621249ef76b00734f12fb61ee28dd8
Jeff Nicholas <jnicholas@hamiltonmfg.com>
2023-06-15 10:56:09 -0400

    🔥 refactor(main.py): remove unused cmdline_args variable ✨ feat(main.py): set app_mode to True to run Eel app in app mode, improving user experience 🐛 fix(main.py): change Eel mode from custom to chrome to fix issue with opening app in Brave browser.

--- main.py
+++ main.py
@@ -84,18 +84,13 @@ def add_hymn(hymn_number: int) -> list[dict]:
 
 def start_eel() -> None:
     '''Starts the Eel server.'''
-    
-    cmdline_args = [
-        'brave-portable/brave-portable.exe', 
-        '--app=http://localhost:8080/index.html'
-    ]
 
     eel.init('web')
     eel.start(
         'index.html', 
-        mode='custom',
-        cmdline_args=cmdline_args,
+        mode='chrome',
         host='localhost',
+        app_mode=True,
         port=8080,
         size=(1600, 1200)
     )
