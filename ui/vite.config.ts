f59f9e63bd41cad14099c3eec272b9f6c91d5424
Jeff Nicholas <jefnic23@gmail.com>
2023-06-16 23:44:45 -0400

    🔧 chore(ui): reorganize paths in tsconfig.json and vite.config.ts to improve code organization and maintainability

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import path from 'path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,8 +12,9 @@ export default defineConfig({
 	resolve: {
 		alias: {
 			'@components': path.resolve(__dirname, './src/components'),
-			'@stores': path.resolve(__dirname, './src/stores'),
 			'@interfaces': path.resolve(__dirname, './src/interfaces'),
+            '@pages': path.resolve(__dirname, './src/pages'),
+            '@stores': path.resolve(__dirname, './src/stores')
 		}
 	}
 })
