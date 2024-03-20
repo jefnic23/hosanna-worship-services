725833eacf69ec19c1223e3ef02e59fb0466e5f8
Jeff <jnicholas@hamiltonmfg.com>
2024-03-20 14:09:56 -0400

    cleaned up markdown editor

--- ui/src/components/TextEditor.svelte
+++ ui/src/components/TextEditor.svelte
@@ -3,6 +3,8 @@
     import { marked } from 'marked';
 
     let text: string = '';
+    let htmlContent: string = '';
+    $: text, parseMarkdown(text);
 
     let liturgies: string[] = [];
     eel.list_liturgies()((n: string[]) => {
@@ -17,6 +19,10 @@
         text = (e.target as HTMLTextAreaElement).value;
     }
 
+    async function parseMarkdown(text: string): Promise<void> {
+        htmlContent = await marked.parse(text);
+    }
+
     /**
      * Prints the text
      */
@@ -28,24 +34,39 @@
 {#each liturgies as liturgy (liturgy)}
     <p>{liturgy}</p>
 {/each}
-<div class='flex'>
-    <textarea bind:value={text} on:change={handleTextChange}></textarea>
-    <button on:click={handleSubmit}>Submit</button>
+<div class='flex-row'>
+    <div class='flex-col'>
+        <textarea bind:value={text} on:change={handleTextChange}></textarea>
+        <button on:click={handleSubmit}>Submit</button>
+    </div>
+    <div class='powerpoint'>{@html htmlContent}</div>
 </div>
 
+
 <style>
-    .flex {
+    .flex-row {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+    }
+
+    .flex-col {
         display: flex;
         flex-direction: column;
         justify-content: center;
     }
 
-    textarea {
+    .powerpoint {
         width: 565px;
         height: 335px;
         padding: 31.5px 16.5px;
         resize: none;
         font-size: 24px;
         font-family: 'Segoe UI';
+        line-height: 1.25;
+    }
+
+    :global(.powerpoint *){
+        margin: 0 0;
     }
 </style>
