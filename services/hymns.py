e2c6ff731f46ae4ac3b24593d43100016f47f48f
Jeffrey Nicholas <jefnic23@gmail.com>
2024-03-30 11:34:09 -0400

    📝 (hymns.py): refactor Hymns class to improve code readability and maintainability by renaming variables and methods for better clarity and removing unnecessary comments and methods.

--- services/hymns.py
+++ services/hymns.py
@@ -9,49 +9,14 @@ from services.settings import Settings
 class Hymns:
     """Class for adding hymns to the service."""
 
-    DF = pd.read_csv('data/hymnal.csv')
-
 
     def __init__(self, settings: Settings) -> None:
         self.day: date = date.today()
         self._path: str = f'{settings.LOCAL_DIR}/services'
-        self._hymns: list[Hymn] = []
-    
-
-    def add_hymn(self, hymn_number: int) -> Hymn:
-        """Add hymn to service."""
-        self._hymns.append(hymn := Hymns.lookup_hymn(hymn_number))
-        return hymn
+        self._df: pd.DataFrame = pd.read_csv('data/hymnal.csv')
     
     
-    def add_other_hymn(
-        self,
-        hymn_number: int,
-        hymn_title: str,
-        hymnal: str
-    ) -> Hymn:
-        self._hymns.append(hymn := Hymn(title=f'({hymnal}) {hymn_title}', number=hymn_number))
-        return hymn
-    
-    
-    def get_hymn(self, index: int) -> Hymn:
-        """
-        Get hymn by index.
-        
-        Returns
-        -------
-            Hymn: the requested hymn.
-        """
-        if index > len(self._hymns) or index < 1:
-            raise IndexError(f"Please enter a number between 1 and {len(self._hymns)}")
-        return self._hymns[index - 1]
-
-
-    @staticmethod
-    def lookup_hymn(
-        hymn_number: int, 
-        df: pd.DataFrame = DF
-    ) -> Hymn:
+    def get_hymn(self, hymn_number: int) -> Hymn:
         """
         Lookup hymn in hymnal.
 
@@ -65,6 +30,15 @@ class Hymns:
             Hymn : the title and number of the hymn.
         """
         try:
-            return Hymn(**df.loc[df['number'] == hymn_number].to_dict('records')[0])
+            return Hymn(**self._df.loc[self._df['number'] == hymn_number].to_dict('records')[0])
         except KeyError:
             print(f'Hymn {hymn_number} not found.')
+    
+
+    def get_other_hymn(
+        self,
+        hymn_number: int,
+        hymn_title: str,
+        hymnal: str
+    ) -> Hymn:
+        return Hymn(title=f'({hymnal}) {hymn_title}', number=hymn_number)
