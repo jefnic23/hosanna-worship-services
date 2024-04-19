234497a2661f0d94d21cc229596da2935cab31a5
Jeffrey Nicholas <jefnic23@gmail.com>
2024-04-18 20:53:16 -0400

    ♻️ (transition.ts): refactor fallback function in crossfade transition to include type annotations for better code readability and maintainability

--- ui/src/actions/transition.ts
+++ ui/src/actions/transition.ts
@@ -1,10 +1,10 @@
-import { crossfade } from 'svelte/transition';
+import { crossfade, type CrossfadeParams } from 'svelte/transition';
 import { quintOut } from 'svelte/easing';
 
 export const [send, receive] = crossfade({
     duration: (d) => Math.sqrt(d * 200),
 
-    fallback(node, params) {
+    fallback(node: HTMLElement, params: CrossfadeParams) {
         const style = getComputedStyle(node);
         const transform = style.transform === 'none' ? '' : style.transform;
 
