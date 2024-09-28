89214b9e300f246cf4da30100d509285c4946e65
Jeffrey Nicholas <jefnic23@gmail.com>
2024-09-28 17:30:07 -0400

    test and legacy updates/changes

--- legacy.py
+++ legacy.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 
+from pptx.enum.text import MSO_VERTICAL_ANCHOR
+
 from models.enums.weekday import Weekday
 from services.document import WordDocument
 from services.dropbox import Dropbox
@@ -11,77 +13,162 @@ from services.settings import Settings
 from services.sundaysandseasons import SundaysAndSeasons
 from services.utils import get_day
 
-this_sunday = get_day(delta=0, weekday=Weekday.SUNDAY)
 settings = Settings()
-
-path = Path(settings.LOCAL_DIR, 'services')
-if not os.path.exists(f'{path}/{this_sunday}'):
-    os.makedirs(f'{path}/{this_sunday}')
-
+hymnal = Hymns()
 lit = Liturgy(settings)
-lit.load_files('Lent')
-
-hymns = Hymns(settings)
-hymns.day = this_sunday
-hymns.add_hymn(328)
-hymns.add_hymn(592)
-hymns.add_hymn(793)
-
 sas = SundaysAndSeasons(settings)
-sas.day = this_sunday
-sas.login()
-sas.get_texts_and_images()
-sas.logoff()
+ppt = PowerPoint(settings)
+doc = WordDocument(settings)
+# dbx = Dropbox(settings)
 
-ppt = PowerPoint(settings, sas.title)
-ppt.day = this_sunday
-ppt.convert_image()
-ppt.add_image()
-ppt.add_rich_text('Confession and Forgiveness', lit.confession)
-ppt.add_hymn(hymns.get_hymn(1))
-ppt.add_rich_text('Greeting', lit.greeting)
-ppt.add_rich_text('Kyrie', lit.kyrie, spoken=True)
-ppt.add_rich_text('', lit.lord_be_with_you, anchor='middle')
-ppt.add_rich_text('Prayer of the Day', sas.prayer)
-ppt.add_title_slide(sas.first_reading.title)
-ppt.add_rich_text('First Reading', sas.first_reading.body)
-ppt.add_title_slide(sas.psalm.title)
-ppt.add_rich_text('Psalm', sas.psalm.body)
-ppt.add_title_slide(sas.second_reading.title)
-ppt.add_rich_text('Second Reading', sas.second_reading.body)
-ppt.add_rich_text('Gospel Acclamation', lit.gospel_acclamation, spoken=True)
-ppt.add_gospel_title(sas.gospel.title)
-ppt.add_rich_text('Gospel', sas.gospel.body)
-ppt.add_title_slide('Sermon')
-ppt.add_hymn(hymns.get_hymn(2))
-ppt.add_rich_text('Nicene Creed', lit.apostles_creed, spoken=True)
-ppt.add_intercessions("Lord, in your mercy,", "hear our prayer.")
-ppt.add_rich_text('Dialogue', lit.dialogue, anchor='middle')
-ppt.add_rich_text('Preface', lit.preface)
-ppt.add_rich_text('Holy, holy, holy', lit.canticle, spoken=True)
-ppt.add_rich_text('Thanksgiving', lit.thanksgiving)
-ppt.add_rich_text('Lord\'s Prayer', lit.lords_prayer, spoken=True)
-ppt.add_title_slide('Communion')
-ppt.add_rich_text('Communion Hymn', lit.communion_hymn, spoken=True)
-ppt.add_title_slide('Communion')
-ppt.add_rich_text('Communion', lit.communion_blessing)
-ppt.add_rich_text('Prayer after Communion', lit.prayer_after_communion)
-ppt.add_rich_text('Blessing', lit.blessing)
-ppt.add_hymn(hymns.get_hymn(3))
-ppt.add_rich_text('Dismissal', lit.dismissal, anchor='middle')
-ppt.add_image()
-ppt.save()
 
-doc = WordDocument(settings, sas.title)
-doc.day = this_sunday
-doc.add_rich_text(sas.first_reading)
-doc.add_rich_text(sas.psalm)
-doc.add_rich_text(sas.second_reading)
-doc.save()
+if __name__ == "__main__":
+    # dbx.connect()
+    sas.login()
 
-# dbx = Dropbox(settings, sas.title)
-# dbx.day = this_sunday
-# dbx.connect()
-# dbx.upload('pptx')
-# dbx.upload('pdf')
-# dbx.close()
+    hymn_list = [
+        [532, 414, 434],
+    ]
+
+    for i, hymns in enumerate(hymn_list):
+        day = get_day(delta=i, weekday=Weekday.SUNDAY)
+
+        path = Path(settings.LOCAL_DIR, "services")
+        if not os.path.exists(f"{path}/{day}"):
+            os.makedirs(f"{path}/{day}")
+
+        lit.load_files("Pentecost")
+
+        first_hymn = hymnal.get_hymn(hymns[0])
+        second_hymn = hymnal.get_hymn(hymns[1])
+        third_hymn = hymnal.get_hymn(hymns[2])
+
+        # dbx.day = day
+        doc.day = day
+        ppt.day = day
+        sas.day = day
+
+        sas.get_texts_and_images()
+
+        # region Build PowerPoint
+
+        ppt.convert_image()
+        ppt.add_image()
+        ppt.add_rich_text("Confession and Forgiveness", lit.confession)
+        ppt.add_hymn(first_hymn)
+        ppt.add_rich_text("Greeting", lit.greeting)
+        ppt.add_rich_text("Hymn of Praise", lit.kyrie, spoken=True)
+        ppt.add_rich_text("", lit.lord_be_with_you, anchor=MSO_VERTICAL_ANCHOR.MIDDLE)
+        ppt.add_rich_text("Prayer of the Day", sas.prayer)
+        ppt.add_title_slide(sas.first_reading.title)
+        ppt.add_rich_text("First Reading", sas.first_reading.body)
+        ppt.add_title_slide(sas.psalm.title)
+        ppt.add_rich_text("Psalm", sas.psalm.body)
+        ppt.add_title_slide(sas.second_reading.title)
+        ppt.add_rich_text("Second Reading", sas.second_reading.body)
+        ppt.add_rich_text("Gospel Acclamation", lit.gospel_acclamation, spoken=True)
+        ppt.add_gospel_title(sas.gospel.title)
+        ppt.add_rich_text("Gospel", sas.gospel.body)
+        ppt.add_title_slide("Sermon")
+        ppt.add_hymn(second_hymn)
+        ppt.add_rich_text("Apostles' Creed", lit.apostles_creed, spoken=True)
+        ppt.add_intercessions("Lord, in your mercy,", "hear our prayer.")
+        ppt.add_rich_text("Dialogue", lit.dialogue, anchor=MSO_VERTICAL_ANCHOR.MIDDLE)
+        ppt.add_rich_text("Preface", lit.preface)
+        ppt.add_rich_text("Holy, holy, holy", lit.canticle, spoken=True)
+        ppt.add_rich_text("Thanksgiving", lit.thanksgiving)
+        ppt.add_rich_text("Lord's Prayer", lit.lords_prayer, spoken=True)
+        ppt.add_title_slide("Communion")
+        ppt.add_rich_text("Communion Hymn", lit.communion_hymn, spoken=True)
+        ppt.add_title_slide("Communion")
+        ppt.add_rich_text("Communion", lit.communion_blessing)
+        ppt.add_rich_text("Prayer after Communion", lit.prayer_after_communion)
+        ppt.add_rich_text("Blessing", lit.blessing)
+        ppt.add_hymn(third_hymn)
+        ppt.add_rich_text("Dismissal", lit.dismissal, anchor=MSO_VERTICAL_ANCHOR.MIDDLE)
+        ppt.add_image()
+        ppt.save(sas.title)
+        ppt.reset()
+
+        # endregion
+
+        # region Generate Readings
+
+        doc.add_rich_text(
+            title=f"First Reading: {sas.first_reading.title}",
+            body=sas.first_reading.body,
+            keep_together=True,
+        )
+        doc.add_rich_text(
+            title=sas.psalm.title, body=sas.psalm.body, keep_together=True
+        )
+        doc.add_rich_text(
+            title=f"Second Reading: {sas.second_reading.title}",
+            body=sas.second_reading.body,
+            keep_together=True,
+        )
+        doc.save(filename="Readings", convert_to_pdf=True)
+        doc.reset()
+
+        # endregion
+
+        # region Print Service
+
+        doc.add_rich_text(title="Prelude", body=None, highlight_title=True)
+        doc.add_rich_text(title="Confession and Forgiveness", body=lit.confession)
+        doc.add_rich_text(
+            title=f"Hymn: {first_hymn.title}, ELW {first_hymn.number}",
+            body=None,
+            highlight_title=True,
+        )
+        doc.add_rich_text(title="Greeting", body=lit.greeting)
+        doc.add_rich_text(title="Kyrie", body=None, highlight_title=True)
+        doc.add_rich_text(title=None, body=lit.lord_be_with_you)
+        doc.add_rich_text(title="Prayer of the Day", body=sas.prayer)
+        doc.add_rich_text(
+            title=f"First Reading: {sas.first_reading.title}",
+            body=sas.first_reading.body,
+        )
+        doc.add_rich_text(title=sas.psalm.title, body=sas.psalm.body)
+        doc.add_rich_text(
+            title=f"Second Reading: {sas.second_reading.title}",
+            body=sas.second_reading.body,
+        )
+        doc.add_rich_text(title="Gospel Acclamation", body=None, highlight_title=True)
+        doc.add_rich_text(
+            title=f"Gospel Reading: {sas.gospel.title}", body=sas.gospel.body
+        )
+        doc.add_rich_text(title="Sermon", body=None)
+        doc.add_rich_text(
+            title=f"Hymn: {second_hymn.title}, ELW {second_hymn.number}",
+            body=None,
+            highlight_title=True,
+        )
+        doc.add_rich_text(title="Apostles' Creed", body=lit.apostles_creed)
+        doc.add_rich_text(title="Prayers of Intercession", body=None)
+        doc.add_rich_text(title="Dialogue", body=lit.dialogue)
+        doc.add_rich_text(title="Preface", body=lit.preface)
+        doc.add_rich_text(title="Holy, holy, holy", body=None, highlight_title=True)
+        doc.add_rich_text(title="Lord's Prayer", body=lit.lords_prayer)
+        doc.add_rich_text(title="Communion", body=None, highlight_title=True)
+        doc.add_rich_text(title="Communion Blessing", body=lit.communion_blessing)
+        doc.add_rich_text(
+            title="Prayer after Communion", body=lit.prayer_after_communion
+        )
+        doc.add_rich_text(title="Blessing", body=lit.blessing)
+        doc.add_rich_text(
+            title=f"Hymn: {third_hymn.title}, ELW {third_hymn.number}",
+            body=None,
+            highlight_title=True,
+        )
+        doc.add_rich_text(title="Dismissal", body=lit.dismissal)
+        doc.save(filename=sas.title, convert_to_pdf=True)
+        doc.reset()
+
+        # endregion
+
+        # dbx.upload(f"{sas.title}.docx")
+        # dbx.upload(f"{sas.title}.pptx")
+
+    sas.logoff()
+    # dbx.close()
--- test.py
+++ test.py
@@ -1,189 +1,36 @@
-import re
+text = """
+    "Gathering Song": "Song",
+    Greeting: "Text",
+    Kyrie: "Text",
+    "Prayer of the Day": "Text",
+    "First Reading": "Text",
+    Psalm: "Text",
+    "Second Reading": "Text",
+    "Gospel Acclamation": "Text",
+    Gospel: "Text",
+    Sermon: "Text",
+    "Song of the Day": "Song",
+    Creed: "Text",
+    "Prayers of Intercession": "Text",
+    Dialogue: "Text",
+    Preface: "Text",
+    "Holy, Holy, Holy": "Text",
+    Thanksgiving: "Text",
+    "Lord's Prayer": "Text",
+    Communion: "Text",
+    "Communion Song": "Text",
+    "Communion Blessing": "Text",
+    "Prayer after Communion": "Text",
+    Blessing: "Text",
+    "Sending Song": "Song",
+    Dismissal: "Text",
+"""
 
-from boltons.iterutils import split
-from bs4 import BeautifulSoup
-from PIL import Image, ImageDraw, ImageFont
-from PIL.ImageFont import FreeTypeFont
-
-from models.reading import Reading
-from services.powerpoint import PowerPoint
-from services.sundaysandseasons import SundaysAndSeasons
-from services.utils import clean_text, split_formatted_text
-
-###############################
-
-MAX_WIDTH: int = 565
-MAX_HEIGHT: int = 335
-
-REGULAR: FreeTypeFont = ImageFont.truetype('%SystemRoot%\Fonts\segoeui.ttf', 24)
-BOLD: FreeTypeFont = ImageFont.truetype('%SystemRoot%\Fonts\segoeuib.ttf', 24)
-ITALIC: FreeTypeFont = ImageFont.truetype('%SystemRoot%\Fonts\segoeuii.ttf', 24)
-DRAW: ImageDraw.ImageDraw = ImageDraw.Draw(Image.new('RGB', (MAX_WIDTH, MAX_HEIGHT)))
-
-# def get_height(
-#     lines: str, 
-#     draw: ImageDraw.ImageDraw, 
-#     font: FreeTypeFont, 
-#     max_height: int = MAX_HEIGHT
-# ) -> list[str]:
-#     '''Gets the height of a line of text and splits it if it's too long.'''
-#     if PowerPoint.check_size(lines, draw, font)['height'] < max_height:
-#         return [lines]
-
-#     regex = re.search(r'<div>(.*?)</div>', lines, re.MULTILINE | re.DOTALL)
-#     group = regex.group(1).strip().splitlines() if regex else None
-#     slides = []
-#     for line in lines.splitlines():
-#         if re.match(r'<br>', line):
-#             # If the line is a break, insert a new slide
-#             slides += ['']
-#             continue
-
-#         if re.match(r'<div>', line) or re.match(r'</div>', line):
-#             continue
-
-#         height = PowerPoint.check_size(
-#             '\n'.join(slides[-1:] + [line]), 
-#             draw, 
-#             font
-#         )['height']
-        
-#         if regex is not None and line == group[0]:
-#             # check if the call and response will fit on the current slide
-#             height = PowerPoint.check_size(
-#                 '\n'.join(slides[-1:] + [line] + [group[1]]),
-#                 draw,
-#                 font
-#             )['height']
-#             if height > max_height:
-#                 slides += [line]
-#             else:
-#                 slides[-1:] = ['\n'.join(slides[-1:] + [line])]
-#             continue
-
-#         if height < max_height:
-#             slides[-1:] = ['\n'.join(slides[-1:] + [line]).lstrip()]
-#         else:
-#             slides += [line]
-#     return slides
-
-def get_height(
-    lines: str, 
-    draw: ImageDraw.ImageDraw, 
-    font: ImageFont.FreeTypeFont, 
-    max_height: int = MAX_HEIGHT
-) -> list[str]:
-    '''Gets the height of a line of text and splits it if it's too long.'''
-    if PowerPoint.check_size(lines, draw, font)['height'] < max_height:
-        return [lines]
-
-    # Split the input lines into chunks separated by '<div>' and '</div>'
-    chunks = re.split(r'(<div>.*?</div>)', lines)
-    
-    # Initialize the list of slides
-    slides = ['']
-    
-    for chunk in chunks:
-        # Skip empty chunks
-        if not chunk:
-            continue
-        
-        if chunk.startswith('<div>'):
-            # If it's a '<div>' tag, remove the tags and add it to the current slide
-            slide_text = chunk[len('<div>'):-len('</div>')].replace('<div>', '').replace('</div>', '').strip() 
-            slides[-1] += slide_text
-        else:
-            # If it's not a '<div>' tag, split it by line breaks
-            chunk_lines = chunk.strip().splitlines()
-            for line in chunk_lines:
-                line = line.replace('<div>', '').replace('</div>', '').strip()  # Remove leading and trailing whitespace
-                if PowerPoint.check_size('\n'.join([slides[-1], line]), draw, font)['height'] < max_height:
-                    slides[-1] += '\n' + line
-                else:
-                    slides.append(line)
-    
-    return slides
-
-###############################
-
-with open('test.html', 'rb') as f:
-    soup = BeautifulSoup(f.read(), 'html.parser')
-
-regex = re.compile(r'Psalm:')
-
-parent = soup.find('h3', string=regex)
-title = parent.get_text().split('Psalm: ')[1]
-psalm = parent.find_next_sibling().find_next_sibling()
-
-for br in psalm.find_all('br'):
-    br.replace_with('\n')
-superscripts = SundaysAndSeasons._get_superscripts(psalm)
-text = SundaysAndSeasons._add_superscripts(
-    '\n'.join([
-        clean_text(span.get_text())
-        for span in psalm.find_all('span', {'class': None}) 
-        if span.get_text() and 'style' not in span.attrs 
-    ]),
-    superscripts
-)
-
-bold_text = [clean_text(strong.get_text()) for strong in psalm.find_all('strong')]
-formatted_text = []
-for i, line in enumerate(text.splitlines()):
-    if line.replace('<sup>', '').replace('</sup>', '') in superscripts:
-        try:
-            if text.splitlines()[i+1] in bold_text or text.splitlines()[i-1] in bold_text and i != 0:
-                formatted_text.append(None)
-        except IndexError:
-            pass
-        formatted_text.append(line) 
-    else:
-        formatted_text.append(line)
-
-body = []
-for i, line in enumerate(split(formatted_text, lambda x: x is None)):
-    if line:
-        new_line = SundaysAndSeasons._remove_whitespace(f"{line[0]}{' '.join(line[1:])}")
-        if i % 2 == 0:
-            body.append(f'<div>{new_line}</div>')
-        else:
-            body.append(f'<div><b>{new_line}</b></div>')
-
-reading = Reading(
-    title = title,
-    body = '\n'.join(body)
-)
-
-text = reading.body
-
-superscripts = re.findall(r'<sup>(.*?)</sup>', text)
-regular_text, bold_text, italic_text = split_formatted_text(text.replace('<sup>', '').replace('</sup>', ''))
-bold_lines = [line[1] for line in bold_text]
-italic_lines = [line[1] for line in italic_text]
-lines = [
-    line[1] for line in sorted(regular_text + bold_text + italic_text, key=lambda x: x[0])
-]
-width_formatted_text = []
-bold_formatted_text = []
-italic_formatted_text = []
-for line in lines:
-    if line in bold_lines:
-        bold_line = PowerPoint.get_width(line, DRAW, BOLD)
-        width_formatted_text.append(bold_line)
-        for l in bold_line.splitlines():
-            bold_formatted_text.append(l)
-    elif line in italic_lines:
-        italic_line = PowerPoint.get_width(line, DRAW, ITALIC)
-        width_formatted_text.append(italic_line)
-        for l in italic_line.splitlines():
-            italic_formatted_text.append(l)
-    else:
-        width_formatted_text.append(PowerPoint.get_width(line, DRAW, REGULAR))
-
-width_formatted_text = '\n'.join(width_formatted_text)
-
-slides = get_height(width_formatted_text, DRAW, REGULAR)
-
-for slide in slides:
-    print(slide)
-
+for line in text.strip().splitlines():
+    print(
+        "{name: "
+        + line.strip().split(":")[0]
+        + ", type: ServiceElementType."
+        + line.strip().split(":")[1].split(",")[0].replace("\"", "").strip()
+        + "},"
+    )
